53673193bb5d34d66492675a97bd96986e86a340
courserav <lvsmlit@gmail.com>
2020-12-16 05:07:17 -0500

    commit tree 488cd2cebdc19bff36fdc2c6fa75171a47c34348

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+    i = 0
+    collect_array = []
+
+    while i < array.length
+        collect_array << yield(array[i])
+        i += 1
+    end
+
+    return collect_array
+end
 
 
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
+    name.split(" ").first
+end
+
+my_collect(["ruby", "js", "python"]) do |language|
+    language.upcase
+end
